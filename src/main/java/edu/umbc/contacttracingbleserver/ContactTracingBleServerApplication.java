e0cddee9f015e2fcb9bbc021b94f180fd0f41bf5
rishabh-sachdeva <rishabh.sachdeva0808@gmail.com>
2020-06-23 23:46:22 -0400

    initial project with H2 db

---
+++ src/main/java/edu/umbc/contacttracingbleserver/ContactTracingBleServerApplication.java
@@ -0,0 +1,13 @@
+package edu.umbc.contacttracingbleserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactTracingBleServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactTracingBleServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/umbc/contacttracingbleserver/ContactTracingBleServerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.umbc.contacttracingbleserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactTracingBleServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
